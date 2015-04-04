027efc0bbc7a45ebf64d98ad1193f0b782425d0c
Grammka <grammka@gmail.com>
2015-04-04 15:46:57 +0300

    #? progress

--- example/puls-tour.js
+++ example/puls-tour.js
@@ -24,26 +24,26 @@
 
 	// ==========================================================================
 	
-	function getPointNearElementPosition(options, elmBounds) {
+	function getPointNearElementPosition(cssOptions, elmBounds) {
 		if (this.placement == 'left' || this.placement == 'right') {
-			options.top = elmBounds.top + (elmBounds.height - OPTIONS.pointSize) / 2;
+			cssOptions.top = elmBounds.top + (elmBounds.height - OPTIONS.pointSize) / 2;
 		} else if (this.placement == 'top' || this.placement == 'bottom') {
-			options.left = elmBounds.left + (elmBounds.width - OPTIONS.pointSize) / 2;
+			cssOptions.left = elmBounds.left + (elmBounds.width - OPTIONS.pointSize) / 2;
 		}
 
 		if (this.placement == 'top') {
-			options.top = elmBounds.top - OPTIONS.pointSize / 2;
+			cssOptions.top = elmBounds.top - OPTIONS.pointSize / 2;
 		} else if (this.placement == 'right') {
-			options.left = elmBounds.left + elmBounds.width - OPTIONS.pointSize / 2;
+			cssOptions.left = elmBounds.left + elmBounds.width - OPTIONS.pointSize / 2;
 		} else if (this.placement == 'bottom') {
-			options.bottom = elmBounds.top + elmBounds.height - OPTIONS.pointSize / 2;
+			cssOptions.bottom = elmBounds.top + elmBounds.height - OPTIONS.pointSize / 2;
 		} else if (this.placement == 'left') {
-			options.left = elmBounds.left - OPTIONS.pointSize / 2;
+			cssOptions.left = elmBounds.left - OPTIONS.pointSize / 2;
 		}
 	}
 
 	function getStatesPositions() {
-		var elmBounds,
+		var elmBounds, appendToBounds,
 			positions = {
 				fixedCenter: {
 					position: 'fixed',
@@ -72,53 +72,85 @@
 		// absoluteNearElement ---------------------------------------------------------------- /
 
 		elmBounds = this.$elm[0].getBoundingClientRect();
+		appendToBounds = this.$parent[0].getBoundingClientRect();
 
-		if (this.$parent) {
-			var appendToBounds = this.$parent[0].getBoundingClientRect();
-
-			elmBounds = {
-				width:  elmBounds.width,
-				height: elmBounds.height,
-				top:    elmBounds.top - appendToBounds.top,
-				left:   elmBounds.left - appendToBounds.left,
-				right:  appendToBounds.right - elmBounds.right,
-				bottom: appendToBounds.bottom - elmBounds.bottom
-			};
-		}
+		elmBounds = {
+			width:  elmBounds.width,
+			height: elmBounds.height,
+			top:    elmBounds.top - appendToBounds.top,
+			left:   elmBounds.left - appendToBounds.left,
+			right:  appendToBounds.right - elmBounds.right,
+			bottom: appendToBounds.bottom - elmBounds.bottom
+		};
 
 		getPointNearElementPosition.bind(this)(positions.absoluteNearElement, elmBounds);
 
 		// fixedNearElement ------------------------------------------------------------------- /
 
-		if (this.$parent) {
-			elmBounds = this.$elm[0].getBoundingClientRect();
+		elmBounds = this.$elm[0].getBoundingClientRect();
 
-			getPointNearElementPosition.bind(this)(positions.fixedNearElement, elmBounds);
-		} else {
-			positions.fixedNearElement.top = positions.absoluteNearElement.top;
-			positions.fixedNearElement.left = positions.absoluteNearElement.left;
-		}
+		getPointNearElementPosition.bind(this)(positions.fixedNearElement, elmBounds);
 
 
 		return positions;
 	}
 
-	function PulsTourPoint(id, $elm) {
-		this.point = points[id];
-		this.$elm = $elm;
-		this.placement = $elm.attr('puls-tour-placement');
-		this.message = $elm.attr('puls-tour-message');
-		this.$parent = $elm.attr('puls-tour-parent') ? $($elm.attr('puls-tour-parent')) : null;
-		this.statesPositions = getStatesPositions.bind(this)();
+	// ==========================================================================
 
-		$elm[0].pulsTourPoint = this.point = points[id];
-
-		this.draw();
+	function PulsTourTooltip(point) {
+		this.point = point;
+		this.$tooltip = $('<div class="puls-tour__tooltip">' + point.$elm.attr('puls-tour-message') + '</div>').appendTo(point.$point)
 	}
 
-	PulsTourPoint.prototype.draw = function() {
-		this.$point = $('<div class="puls-tour__point"></div>').appendTo(this.$parent || $('body'));
+	PulsTourTooltip.prototype.setPosition = function() {
+		var tooltipWidth, tooltipHeight;
+		var cssOptions = {};
+
+		tooltipWidth    = this.$tooltip.outerWidth(true);
+		tooltipHeight   = this.$tooltip.outerHeight(true);
+
+		if (this.point.placement == 'left' || this.point.placement == 'right') {
+			cssOptions.top = (OPTIONS.pointSize - tooltipHeight) / 2;
+		} else if (this.point.placement == 'top' || this.point.placement == 'bottom') {
+			cssOptions.left = (OPTIONS.pointSize - tooltipWidth) / 2;
+		}
+
+		if (this.point.placement == 'top') {
+			cssOptions.bottom = '100%';
+		} else if (this.point.placement == 'right') {
+			cssOptions.left = '100%';
+		} else if (this.point.placement == 'bottom') {
+			cssOptions.top = '100%';
+		} else if (this.point.placement == 'left') {
+			cssOptions.right = '100%';
+		}
+
+		this.$tooltip.removeAttr('style').css(cssOptions);
 	};
+
+	PulsTourTooltip.prototype.show = function() {
+		this.point.$point.addClass('puls-tour__point_tooltip-visible');
+		this.setPosition();
+	};
+
+	// ==========================================================================
+
+	function PulsTourPoint(id, $elm) {
+		var point = this;
+
+		this.$elm = $elm;
+		this.placement = $elm.attr('puls-tour-placement');
+		this.$parent = $elm.attr('puls-tour-parent') ? $($elm.attr('puls-tour-parent')) : $('body');
+		this.$point = $('<div class="puls-tour__point"></div>').appendTo(this.$parent);
+		this.tooltip = new PulsTourTooltip(this);
+		this.statesPositions = getStatesPositions.bind(this)();
+
+		$elm[0].pulsTourPoint = points[id];
+
+		this.$point.on('click', function() {
+			point.tooltip.show();
+		});
+	}
 
 	PulsTourPoint.prototype.setPosition = function(step) {
 		this.$point.removeAttr('style').css(this.statesPositions[STATES[step]]);
